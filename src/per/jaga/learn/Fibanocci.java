68eb1aba0bcef80e7a59399b579b492d955a3487
J.Kanagasabapathi <J.Kanagasabapathi@Jagadeesh-Lap.edh.rsystems.com>
2016-11-06 17:02:48 -0800

    Fib logic added

--- src/per/jaga/learn/Fibanocci.java
+++ src/per/jaga/learn/Fibanocci.java
@@ -4,7 +4,19 @@ public class Fibanocci {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+		int a=0, b=1, c, i;
+		
+		System.out.println(a+" "+b);
+		
+		for(i=2; i<=10; i++)
+		{
+			c=a+b;
+			System.out.println(" "+c);
+			a=b;	
+			b=c;
+		}
 	}
 
 }
